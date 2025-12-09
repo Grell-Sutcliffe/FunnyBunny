377b491fb88ab4418469db85bd108f02a2e37294
Jenny Akhtiamova <zhanna.axt@bk.ru>
2025-12-10 00:42:25 +0300

    УРАААААААА

--- FunnyBunny/Assets/Scripts/StuffScripts/MovementScript.cs
+++ FunnyBunny/Assets/Scripts/StuffScripts/MovementScript.cs
@@ -10,12 +10,9 @@ public abstract class MovementScript : MonoBehaviour
     public GameObject movement_points_GO;
     public GameObject root;
 
-    //protected List<GameObject> list_of_movement_points;
-    //protected List<Point> list_of_movement_points;
     protected List<Activities> list_of_movement_points;
     public float move_speed = 1f;
     public float chase_speed = 0.6f;
-    //public float wait_on_point = 0.5f;
 
     protected int current_point_index;
     protected bool need_to_move = true;
@@ -23,7 +20,7 @@ public abstract class MovementScript : MonoBehaviour
     protected bool is_chasing = false;
 
     protected float speed = 1f;
-    protected float stop_distance = 0f;
+    protected float stop_distance = 0.03f;
     protected bool rotate_towards = false;
 
     public bool was_bunny_hit = false;
@@ -42,13 +39,16 @@ public abstract class MovementScript : MonoBehaviour
     protected Vector2 step = Vector2.zero;
 
     Coroutine moveCoroutine;
-    
+
     protected SpriteRenderer sprite;
 
     protected GameObject target;
 
+    protected Rigidbody2D rb;
+
     protected virtual void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         animator = root.GetComponent<Animator>();
         sprite = root.GetComponent<SpriteRenderer>();
 
@@ -64,9 +64,9 @@ public abstract class MovementScript : MonoBehaviour
     {
         if (need_to_move && is_chasing && player != null)
         {
-            //Debug.Log("Chase");
             target = player_GO;
 
+            /*
             Vector3 targetPos = player.position;
 
             transform.position = Vector3.MoveTowards(
@@ -74,21 +74,21 @@ public abstract class MovementScript : MonoBehaviour
                 targetPos,
                 chase_speed * Time.deltaTime
             );
+            */
         }
     }
 
     protected virtual void FixedUpdate()
     {
-        if (!need_to_move)
+        if (!need_to_move || target == null)
         {
             animator.SetBool(is_walking, false);
             return;
         }
 
-        //Vector2 toTarget = (Vector2)target.transform.position - rb.position;
         Vector2 toTarget = (Vector2)target.transform.position - (Vector2)this.transform.position;
         float dist = toTarget.magnitude;
-        //Debug.Log($"{toTarget}, target.transform.position = {(Vector2)target.transform.position} dist = {dist}");
+
         if (dist <= stop_distance)
         {
             animator.SetBool(is_walking, false);
@@ -107,11 +107,7 @@ public abstract class MovementScript : MonoBehaviour
         }
         step = toTarget.normalized * speed * Time.fixedDeltaTime;
 
-        // SetDirection(step);
-
-        //rb.MovePosition(rb.position + step);
-
-        //if (rotate_towards && step.sqrMagnitude > 0f) rb.rotation = Mathf.Atan2(step.y, step.x) * Mathf.Rad2Deg;
+        if (rb != null) rb.MovePosition(rb.position + step);
     }
 
     protected float GetAngle(Vector2 vector)
@@ -127,7 +123,6 @@ public abstract class MovementScript : MonoBehaviour
     protected void Flip()
     {
         sprite.flipX = !sprite.flipX;
-        //sprite.flipY = !sprite.flipY;
     }
 
     protected virtual void OnTriggerEnter2D(Collider2D other)
@@ -169,29 +164,17 @@ public abstract class MovementScript : MonoBehaviour
         while (true)
         {
             GameObject targetPoint = list_of_movement_points[current_point_index].GetPoint().point_GO;
-
             target = targetPoint;
 
-            Vector3 targetPos = targetPoint.transform.position;
-
-            while (Vector3.Distance(transform.position, targetPos) > 0.01f)
+            while (Vector2.Distance(transform.position, targetPoint.transform.position) > stop_distance)
             {
-                if (need_to_move)
-                {
-                    transform.position = Vector3.MoveTowards(
-                        transform.position,
-                        targetPos,
-                        move_speed * Time.deltaTime
-                    );
-                }
-
                 yield return null;
             }
 
-            transform.position = targetPos;
-
             yield return new WaitForSeconds(list_of_movement_points[current_point_index].GetPoint().wait_time);
-            list_of_movement_points[current_point_index].Trigger();// ??????
+
+            list_of_movement_points[current_point_index].Trigger();
+
             current_point_index = (current_point_index + 1) % list_of_movement_points.Count;
         }
     }
@@ -221,32 +204,27 @@ public abstract class MovementScript : MonoBehaviour
         if (list_of_movement_points.Count == 0)
             yield break;
 
-        int nearestIndex = GetNearestPointIndex();
-        GameObject targetPoint = list_of_movement_points[nearestIndex].GetPoint().point_GO;
+        if (current_point_index < 0 || current_point_index >= list_of_movement_points.Count)
+            current_point_index = 0;
 
-        target = targetPoint;
-
-        Vector3 targetPos = targetPoint.transform.position;
-
-        while (Vector3.Distance(transform.position, targetPos) > 0.01f)
+        while (true)
         {
-            if (need_to_move)
+            var pointData = list_of_movement_points[current_point_index].GetPoint();
+            GameObject targetPoint = pointData.point_GO;
+
+            target = targetPoint;
+
+            while (Vector2.Distance(transform.position, targetPoint.transform.position) > stop_distance)
             {
-                transform.position = Vector3.MoveTowards(
-                    transform.position,
-                    targetPos,
-                    move_speed * Time.deltaTime
-                );
+                yield return null;
             }
 
-            yield return null;
+            yield return new WaitForSeconds(pointData.wait_time);
+
+            list_of_movement_points[current_point_index].Trigger();
+
+            current_point_index = (current_point_index + 1) % list_of_movement_points.Count;
         }
-
-        transform.position = targetPos;
-
-        current_point_index = nearestIndex;
-
-        moveCoroutine = StartCoroutine(MoveAlongPointsLoop());
     }
 
     protected void FillListOfMovementPoints()
