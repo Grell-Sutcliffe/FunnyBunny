8dc04164d29875b8db5b7e1e9de016c7508f8850
VaksHSE <macick@mail.ru>
2025-12-10 02:55:44 +0300

    dedus lay the key

---
+++ FunnyBunny/Assets/Scripts/asdasd.cs
@@ -0,0 +1,43 @@
+using System.Drawing;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class asdasd : Bebebe, Activities, IPointerDownHandler
+{
+    [SerializeField]
+    float timer = 0.5f;
+
+    public bool IsActive()
+    {
+        return false;
+    }
+  
+    public int GetKey()
+    {
+        return 0;
+    }
+    public void MakeActive()
+    {
+    }
+    public void Trigger()
+    {
+        Instantiate(mainController.RetPref(2), transform.position, Quaternion.identity);
+    }
+
+    public Point GetPoint()
+    {
+        return point;
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("MP");
+
+    }
+    void Start()
+    {
+        point = new Point(gameObject, timer);
+        base.Start();
+    }
+    
+  
+}
