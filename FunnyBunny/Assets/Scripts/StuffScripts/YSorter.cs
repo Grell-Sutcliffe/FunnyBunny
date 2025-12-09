19de12f3777a8ddd7f5171e6bd0a3e9705cdc4e1
Jenny Akhtiamova <zhanna.axt@bk.ru>
2025-12-09 03:57:03 +0300

    dedfef

--- FunnyBunny/Assets/Scripts/StuffScripts/YSorter.cs
+++ FunnyBunny/Assets/Scripts/StuffScripts/YSorter.cs
@@ -1,14 +1,17 @@
 using UnityEngine;
 
-[RequireComponent(typeof(Renderer))]
+//[RequireComponent(typeof(Renderer))]
+[RequireComponent(typeof(SpriteRenderer))]
 public class YSorter : MonoBehaviour
 {
     public int baseOrder = 0;
     public int mul = 10;
 
-    Renderer r;
+    //Renderer r;
+    SpriteRenderer r;
 
-    void Awake() => r = GetComponent<Renderer>();
+    //void Awake() => r = GetComponent<Renderer>();
+    void Awake() => r = GetComponent<SpriteRenderer>();
 
     void LateUpdate()
     {
